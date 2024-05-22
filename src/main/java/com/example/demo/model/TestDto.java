ead3f81de75792f7ac37a559ccb87b894fe4df95
swon98 <installous06@naver.com>
2024-05-22 21:04:15 +0900

    setting

---
+++ src/main/java/com/example/demo/model/TestDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestDto {
+    private String grade;
+    private String level;
+    private String station;
+    private String fineDust;
+    private String ultrafineDust;
+    private String date;
+}
